603e41501cad5412b3fee59ce6b8d914bd0cc2e9
“anka-wojcik <anka.wojcik@yahoo.com>
2018-07-15 22:09:05 +0200

    Update

--- src/main/java/com/crud/tasks/domain/CardBadges/AttachementsByTypeDto.java
+++ src/main/java/com/crud/tasks/domain/CardBadges/AttachementsByTypeDto.java
@@ -1,5 +1,5 @@
 package com.crud.tasks.domain.CardBadges;
-
+//
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
